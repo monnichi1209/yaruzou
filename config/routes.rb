f450f9303c4b2164e07f6202a83fa8ec7206b157
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-08-17 18:40:56 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'pages/home'
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # devise_scopeを追加
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
+  end
+
   resources :tasks
   root 'pages#home'
 
