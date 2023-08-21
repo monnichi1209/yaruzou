5f97b34a70a99a752d00981f8519f3a3c38f33ba
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-08-21 14:26:38 +0900

    フロントデザイン 言葉のチョイス

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.unique.email }
+    password { 'password123' }
+    password_confirmation { 'password123' }
+    # その他の属性も追加できます
   end
 end
