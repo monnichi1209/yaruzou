1e5522be725cdd64012383eac0c609cd00944435
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-08-24 01:01:09 +0900

    ゲストログイン問題解決

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.name = "Guest User"
     end
     sign_in user
     redirect_to tasks_path, notice: 'ゲストユーザーとしてログインしました。'
@@ -12,6 +13,7 @@ class Users::SessionsController < Devise::SessionsController
     admin = User.find_or_create_by!(email: 'admin_guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.admin = true
+      user.name = "Guest User"  # ここで name 属性に値を設定
     end
     sign_in admin
     redirect_to rails_admin.dashboard_path, notice: '管理者ゲストユーザーとしてログインしました。'
