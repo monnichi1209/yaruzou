56c197862c2aa637d41aa93d7c7a64096114f7c1
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-08-23 16:42:21 +0900

    サイドメニュー アカウント編集非表示

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  before_action :hide_sidebar, only: [:new, :edit, :update]
 
   protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email_confirmation])
   end
-end
+
+    def hide_sidebar
+      @hide_sidebar = true
+    end
+  end
+ 
