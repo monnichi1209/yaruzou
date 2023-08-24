74fe8aa2a38e8a43feccdc211c132f4a179d22dc
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-08-24 13:56:13 +0900

    テストコード実行準備段階

--- spec/systems/task_spec.rb
+++ spec/systems/task_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     before do
       user = FactoryBot.create(:user)
-      visit login_path
+      visit new_user_session_path
       fill_in 'email', with: user.email
       fill_in 'password', with: user.password
       click_button 'Submit'
